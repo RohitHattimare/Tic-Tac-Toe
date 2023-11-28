ba384df7dff6391b16efe93bba8106439cac6496
xpknight <xpknight@gmail.com>
2023-11-28 18:01:59 +0530

    Tic-Tac-Toe Updated -Table and state change implimented -Remaining Tasks: User Switching remaining & Maintainig Log

---
+++ TIc-tac-toe/src/Square.js
@@ -0,0 +1,10 @@
+//Single Component for the square in board
+
+export default function Square({ value,onSquareClick }) {
+
+    return <>
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    </>
+}
